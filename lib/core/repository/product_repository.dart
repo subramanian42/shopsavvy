03277c726e1458f952022ad53dfc1a44158d87a4
subramanian42 <88108372+subramanian42@users.noreply.github.com>
2023-06-29 15:10:31 +0530

    feat: create product repository

---
+++ lib/core/repository/product_repository.dart
@@ -0,0 +1,25 @@
+import '../clients/punk_api_client.dart';
+import '../model/beer_model.dart';
+
+class ProductRepository {
+  final PunkApiClient _punkApiClient;
+  ProductRepository(this._punkApiClient);
+
+  Future<List<Beer>> fetchProductList(int page) async {
+    final List<Map<String, dynamic>> result = await _punkApiClient.fetchList(
+      "/beers",
+      queryParams: {
+        'page': page.toString(),
+      },
+    ).then(
+      (List<dynamic> value) =>
+          value.map((dynamic e) => e as Map<String, dynamic>).toList(),
+    );
+
+    final List<Beer> productList = <Beer>[];
+    for (Map<String, dynamic> element in result) {
+      productList.add(Beer.fromJson(element));
+    }
+    return productList;
+  }
+}
