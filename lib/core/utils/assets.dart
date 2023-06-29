1ed26ad845dd2d4f781638fe9b1adb0d9b720e34
subramanian42 <88108372+subramanian42@users.noreply.github.com>
2023-06-29 15:11:28 +0530

    feat: add asset name to assets class

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,17 @@
+class Assets {
+  static const appIconSmall = "assets/app_logo/logo.png";
+  static const appIconMedium = "assets/app_logo/logo@2x.png";
+  static const appIconLarge = "assets/app_logo/logo@3x.png";
+  static const googleIconSmall = "assets/google_logo/ic_google.png";
+  static const googleIconMedium = "assets/google_logo/ic_google@2x.png";
+  static const googleIconLarge = "assets/google_logo/ic_google@3x.png";
+  static const facebookIconSmall = "assets/facebook_logo/ic_facebook.png";
+  static const facebookIconMedium = "assets/facebook_logo/ic_facebook@2x.png";
+  static const facebookIconLarge = "assets/facebook_logo/ic_facebook@3x.png";
+  static const linkedinIconSmall = "assets/linkedin_logo/ic_linkedin.png";
+  static const linkedinIconMedium = "assets/linkedin_logo/ic_linkedin@2x.png";
+  static const linkedinIconLarge = "assets/linkedin_logo/ic_linkedin@3x.png";
+  static const profile = "assets/icons/profile_icon.png";
+  static const backButton = "assets/icons/back_button_icon.png";
+  static const background = "assets/icons/background_icon.png";
+}
