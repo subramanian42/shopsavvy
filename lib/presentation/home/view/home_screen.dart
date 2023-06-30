2d594d3e59acee776484bd61b93a9d0689bec1d8
subramanian42 <88108372+subramanian42@users.noreply.github.com>
2023-06-30 23:44:44 +0530

    fix: dispose gridviewcontroller

--- lib/presentation/home/view/home_screen.dart
+++ lib/presentation/home/view/home_screen.dart
@@ -122,6 +122,7 @@ class _HomeScreenState extends State<HomeScreen> {
     _scrollController
       ..removeListener(_onScroll)
       ..dispose();
+    _gridScrollController.dispose();
     super.dispose();
   }
 
