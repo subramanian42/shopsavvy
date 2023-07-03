48234154ee50d827e88f9ae8a9d03c8788d27641
subramanian42 <88108372+subramanian42@users.noreply.github.com>
2023-07-04 00:49:02 +0530

    feat: add droppable transformer to avoid unnecessary calls

--- lib/presentation/home/controller/home_bloc.dart
+++ lib/presentation/home/controller/home_bloc.dart
@@ -1,16 +1,28 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopsavvy/core/repository/product_repository.dart';
-
+import 'package:bloc_concurrency/bloc_concurrency.dart';
+import 'package:stream_transform/stream_transform.dart';
 import '../../../core/model/beer_model.dart';
 part 'home_event.dart';
 part 'home_state.dart';
+
+const throttleDuration = Duration(milliseconds: 100);
+
+EventTransformer<E> throttleDroppable<E>(Duration duration) {
+  return (events, mapper) {
+    return droppable<E>().call(events.throttle(duration), mapper);
+  };
+}
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final ProductRepository productRepository;
   HomeBloc({
     required this.productRepository,
   }) : super(HomeState()) {
-    on<FetchItems>(_onFetchItems);
+    on<FetchItems>(
+      _onFetchItems,
+      transformer: throttleDroppable(throttleDuration),
+    );
   }
   void _onFetchItems(FetchItems event, Emitter<HomeState> emit) async {
     try {
